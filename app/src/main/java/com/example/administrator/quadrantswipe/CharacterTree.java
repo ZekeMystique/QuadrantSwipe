34e9be56b26475d767dae3f569027861f2bfdbef
ZekeMystique <laserbeast@gmail.com>
2015-11-30 10:34:30 -0700

    Number and layout functionality added.  There's a bug with layout changing when shift key is enabled.

--- app/src/main/java/com/example/administrator/quadrantswipe/CharacterTree.java
+++ app/src/main/java/com/example/administrator/quadrantswipe/CharacterTree.java
@@ -1,5 +1,7 @@
 package com.example.administrator.quadrantswipe;
 
+import android.view.View;
+
 /**
  * Created by Rohan Dawson on 13/11/2015.
  */
@@ -8,27 +10,45 @@ public class CharacterTree {
     TreeNode root;
     TreeNode pointer;
     StringBuffer outputBuffer;
+    boolean isChars;
+    private String[] charMap = new String[]{
+        "i", "a", "l", "h", "o", "n", "r", "s", "m", "u", "e", "t",
+                "q", "z", "j", "x", "v", "w", "p", "f", ",", ".", "y", "g", "b", "k", "c", "d"};
+    private String[] numMap = new String[]{
+            "1", "2", "3", ".",         // top left
+            "4", "5", "6", ",",         // top right
+            "7", "8", "!", "?",         // bottom left
+            "9", "0", "@", "#"};       // bottom right
 
     CharacterTree()
     {
-        this(new String[]{
+        /*this(new String[]{
             "i", "a", "l", "h", "o", "n", "r", "s", "m", "u", "e", "t",
             "q", "z", "j", "x", "v", "w", "p", "f", ",", ".", "y", "g", "b", "k", "c", "d"
-        });
+        });*/
     }
 
-    CharacterTree(String charArray[])
+    //CharacterTree(String charArray[])
+    CharacterTree(boolean chars)
     {
+        String charArray[];
+        if(chars){
+            charArray = charMap;
+        }
+        else{
+            charArray = numMap;
+        }
         //Create array of TreeNode leaves
-        TreeNode[] nodeArray = new TreeNode[28];
+        TreeNode[] nodeArray = new TreeNode[charArray.length];
 
         //Add an assert statement to say charArray.length() = 28
 
         //Initialise all leaves
-        for (int i=0; i<28; i++)
+        for (int i=0; i<charArray.length; i++)
             nodeArray[i] = new TreeNode(charArray[i], true);
 
         //Create and initialise branches
+        if(chars){
         TreeNode tl = new TreeNode(null, false, nodeArray[0], nodeArray[1], nodeArray[2], nodeArray[3]);
         TreeNode tr = new TreeNode(null, false, nodeArray[4], nodeArray[5], nodeArray[6], nodeArray[7]);
         TreeNode bl = new TreeNode(null, false, nodeArray[8], nodeArray[9], nodeArray[10], nodeArray[11]);
@@ -38,8 +58,15 @@ public class CharacterTree {
         br.topRight = new TreeNode(null, false, nodeArray[16], nodeArray[17], nodeArray[18], nodeArray[19]);
         br.bottomLeft = new TreeNode(null, false, nodeArray[20], nodeArray[21], nodeArray[22], nodeArray[23]);
         br.bottomRight = new TreeNode(null, false, nodeArray[24], nodeArray[25], nodeArray[26], nodeArray[27]);
-
-        root = new TreeNode(null, false, tl, tr, bl, br);
+            root = new TreeNode(null, false, tl, tr, bl, br);
+        }
+        else{
+            TreeNode tl = new TreeNode(null, false, nodeArray[0], nodeArray[1], nodeArray[2], nodeArray[3]);
+            TreeNode tr = new TreeNode(null, false, nodeArray[4], nodeArray[5], nodeArray[6], nodeArray[7]);
+            TreeNode bl = new TreeNode(null, false, nodeArray[8], nodeArray[9], nodeArray[10], nodeArray[1]);
+            TreeNode br = new TreeNode(null, false, nodeArray[12], nodeArray[13], nodeArray[14], nodeArray[15]);
+            root = new TreeNode(null, false, tl, tr, bl, br);
+        }
 
         //Initialise pointer
         pointer = root;
@@ -48,7 +75,7 @@ public class CharacterTree {
         outputBuffer = new StringBuffer();
     }
 
-    private class TreeNode
+    public class TreeNode
     {
         String data;
         boolean leaf;
@@ -72,7 +99,13 @@ public class CharacterTree {
             this.bottomLeft = bottomLeft;
             this.bottomRight = bottomRight;
         }
+        public boolean getLeaf(){
+            return pointer.leaf;
+        }
+
     }
+
+
 
     public String onTopLeftSwipe()
     {
@@ -161,5 +194,10 @@ public class CharacterTree {
         }
         return outputBuffer;
     }
+
+    public boolean getLeaf(){
+        return pointer.leaf;
+    }
+
 
 }
